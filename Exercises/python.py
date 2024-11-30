3cca2231a895236dbf10c654981f09436b593198
Drewz1312 <54374158+Drewz1312@users.noreply.github.com>
2024-11-30 22:23:35 +0300

    new file

---
+++ Exercises/python.py
@@ -0,0 +1,31 @@
+#print("Hello World!") #Вывести "Привет мир"
+#input()  #Ввести текст
+
+#print(list("Cool dude")) #лист задать
+#list = {"android": "cool dude"} #задать словарь и вывести только первое слово
+#print(list["android"])
+#input() #задать иначе вылетит python console
+
+#Первая игра на Python
+import random
+right_answer = int()
+user_answer = None
+processed_answer = None
+while True:
+    right_answer = random.randint(1, 100)
+    user_answer = input("Guess the number in diaposone 1 to 100!")
+    try:
+        processed_answer = int(user_answer)
+    except ValueError:
+        print("Invalid input. Please enter a number.")
+        continue
+    if processed_answer == right_answer:
+        print("You Win!")
+        break
+    elif processed_answer < right_answer:
+        print("Too low! Try again.")
+        continue
+    elif processed_answer > right_answer:
+        print("Too high! Try again.")
+        continue
+input()
